59b584ee3dca4e575edfe247e349e38f4aefd216
oritonda <oritonda@thedigitalacademy.co.za>
2021-04-22 16:08:56 +0200

    view books function

--- frontend/src/app/components/view-books/view-books.component.ts
+++ frontend/src/app/components/view-books/view-books.component.ts
@@ -1,3 +1,4 @@
+import { ApiServiceService } from './../../services/api-service.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-books.component.css']
 })
 export class ViewBooksComponent implements OnInit {
+  // variable to contain all the books
+  books:any=[]
 
-  constructor() { }
+  constructor(private api:ApiServiceService) { }
 
   ngOnInit(): void {
+    // calling the get books method 
+    this.getBooks()
+  }
+  // function to get books from the api
+  getBooks()
+  {
+    // subscribing to the book service
+    this.api.getBooks().subscribe(books=>{
+      this.books = books
+
+    })
   }
 
 }
