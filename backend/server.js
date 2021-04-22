4dbc0641b453579dd2b38ea23cfa9162fb2ebd53
oritonda <oritonda@thedigitalacademy.co.za>
2021-04-22 10:26:10 +0200

    creating paths

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,19 @@
 // imports
 //Imports
 const express = require("express");
-// const route = require("./routes/");
+const route = require("./routes/router");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("./database/connection");
-
+// initializing app
 const app = express();
-app.use(cors());
 
 //Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.use('/',route)
 
-// app.use("/route", route);
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
